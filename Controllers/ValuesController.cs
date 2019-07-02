ab4290474fda052aac5d3327f62501e60e402ec7
Idan Shahar <idanshahar@gmail.com>
2019-07-02 09:24:18 +0300

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace core.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "test", "idan"};
+            return new string[] { "value1", "value2", "test"};
         }
 
         // GET api/values/5
